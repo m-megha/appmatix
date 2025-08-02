46622d16687b27cdc3caa356d1c364b8b6548538
m-megha <meghaeranattil@gmail.com>
2025-08-02 18:44:35 +0530

    Implemented header extended section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,35 @@ import A from "/assets/images/A.png";
 import Ellipsis from "/assets/svg/Ellipsis.svg";
 import Navbar from "./Navbar";
 import Actions from "./Actions";
+import ScrollDown from "/assets/svg/ScrollDown.svg";
+import ArrowDown from "/assets/icons/ArrowDown.svg";
 
 const Header = () => {
   return (
-    <div className="header">
-      <Navbar />
-      <Actions />
-      <img className="ellipsis" src={Ellipsis} />
-      <img className="fadedLogo" src={A} />
-      <img className="fadedLogo2" src={A} />
-      <div className="contents">
-        <img src={Appmatix} />
-        <h1 id="subtitle">
-          Lorem ipsum dolor sit amet, <br />
-          consectetur adipiscing elit, sed.
-        </h1>
+    <>
+      <div className="header">
+        <Navbar />
+        <Actions />
+        <img className="ellipsis" src={Ellipsis} />
+        <img className="fadedLogo" src={A} />
+        <img className="fadedLogo2" src={A} />
+        <div className="contents">
+          <img src={Appmatix} />
+          <h1 id="subtitle">
+            Lorem ipsum dolor sit amet, <br />
+            consectetur adipiscing elit, sed.
+          </h1>
+        </div>
+        <div className="scrollIndicator">
+          <img src={ScrollDown} />
+          <div className="arrows">
+            <img src={ArrowDown} />
+            <img src={ArrowDown} />
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="headerExtended" />
+    </>
   );
 };
 
