e2ffa41c2c3a088163a13472d365af207d909639
m-megha <59439730+m-megha@users.noreply.github.com>
2025-08-03 23:03:56 +0530

    Implemented story section

---
+++ src/components/Story.tsx
@@ -0,0 +1,25 @@
+import "./Story.css";
+import Appmatix from "/assets/images/Appmatix.png";
+import A from "/assets/images/A.png";
+
+const Story = () => {
+  return (
+    <div className="story">
+      <div className="storyContents">
+        <img className="logoImage" src={A} />
+        <div className="title">
+          <h1>Story Of</h1>
+          <img src={Appmatix} />
+        </div>
+        <p className="description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Story;
