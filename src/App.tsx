87909750906ecb035eaded846d9b6bcfde3237b1
m-megha <meghaeranattil@gmail.com>
2025-08-03 23:02:01 +0530

    Implemented story section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/About";
 import Header from "./components/Header";
+import Story from "./components/Story";
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Story />
     </>
   );
 }
